066e9782b53aa1fa3f87a70bf484f65afc1ae0d0
Duncan Jauncey <duncan.jauncey@gmail.com>
2012-05-03 21:05:24 +0100

    Bugfix - missing function sendCrashedPacket() - not implemented yet.

--- xdata.c
+++ xdata.c
@@ -183,11 +183,11 @@ PLUGIN_API void XPluginReceiveMessage(
 			sendAircraftPacket();
         }
     }
-	else
-	if( inMessage == XPLM_MSG_PLANE_CRASHED )
-	{
-		sendCrashedPacket();
-	}
+//	else
+//	if( inMessage == XPLM_MSG_PLANE_CRASHED )
+//	{
+//		sendCrashedPacket();
+//	}
 
 }
 
